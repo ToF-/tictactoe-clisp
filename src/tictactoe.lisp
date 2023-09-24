74c03d81c47683b92b1903a32273d3c0254f96f8
ToF_ <cthibauttof@gmail.com>
2023-09-24 21:24:35 +0200

    completed tictactoe game status

--- src/tictactoe.lisp
+++ src/tictactoe.lisp
@@ -1,17 +1,27 @@
 (defun initial-game () nil)
 
 (defun game-status (game)
-  (if (evenp (length game))
-    'x-to-play
-    'o-to-play))
+  (labels ((lines () '((0 1 2) (3 4 5) (6 7 8) (0 3 6) (1 4 7) (2 5 8) (0 4 8) (6 4 2)))
+           (has-cellp (cell player game)
+                        (equalp (cons cell player) (assoc cell game :test #'equalp)))
+           (has-linep (line player game)
+                        (every (lambda (cell) (has-cellp cell player game)) line))
+           (win-p (player game)
+                    (some (lambda (line) (has-linep line player game)) (lines))))
+    (cond 
+      ((win-p 'x game) 'x-win)
+      ((win-p 'o game) 'o-win)
+      ((= 9 (length game)) 'draw)
+      ((evenp (length game)) 'x-to-play)
+      (t 'o-to-play))))
 
 (defun move (game row col)
   (labels ((player (game)
-                   (if (eql 'x-to-play (game-status game))
-                     'x
-                     'o)))
+                   (if (eql 'x-to-play (game-status game)) 'x 'o))
+           (coord (col row)
+                  (+ (1- col) (* 3 (1- row)))))
     (let ((p (player game))
-          (m (cons row col)))
+          (m (coord row col)))
       (if (assoc m game :test #'equal)
         game
         (cons (cons m p) game)))))
--- test/tests.lisp
+++ test/tests.lisp
@@ -1,24 +1,48 @@
 (ql:quickload "fiveam")
-    (in-package :fiveam)
+(in-package :fiveam)
 (def-suite* tictactoe-status)
 
-    (load "./src/tictactoe.lisp")
+(load "./src/tictactoe.lisp")
 
-    (test initially-status-is-x-to-play
-     (is (eql 'x-to-play
-          (game-status (initial-game)))))
+(test initially-status-is-x-to-play
+      (is (eql 'x-to-play
+               (game-status (initial-game)))))
 
-    (test after-one-move-status-is-o-to-play
-     (let ((game (move (initial-game) 1 1)))
-      (is (eql 'o-to-play (game-status game)))))
+(test after-one-move-status-is-o-to-play
+      (let ((game (move (initial-game) 1 1)))
+        (is (eql 'o-to-play (game-status game)))))
 
-    (test after-two-moves-status-is-x-to-play
-     (let ((game (move (move (initial-game) 1 1) 2 1)))
-      (is (eql 'x-to-play (game-status game)))))
+(test after-two-moves-status-is-x-to-play
+      (let ((game (move (move (initial-game) 1 1) 2 1)))
+        (is (eql 'x-to-play (game-status game)))))
 
-    (test after-playing-to-a-square-already-played-game-is-unchanged
-     (let* ((game-1 (move (initial-game) 1 1))
-            (game-2 (move game-1 1 1)))
-      (is (equal game-1 game-2))))
+(test after-playing-on-a-square-already-played-game-is-unchanged
+      (let* ((game-1 (move (initial-game) 1 1))
+             (game-2 (move game-1 1 1)))
+        (is (equal game-1 game-2))))
+
+(test after-x-makes-a-line-the-game-is-won-by-x
+      (let ((game (reduce 
+                    (lambda (g coords)
+                        (move g (car coords) (cadr coords)))
+                    '((1 1) (2 1) (1 2) (2 2) (1 3)) 
+                    :initial-value (initial-game))))
+        (is (eql 'x-win (game-status game)))))
+
+(test after-o-makes-a-line-the-game-is-won-by-o
+      (let ((game (reduce 
+                    (lambda (g coords)
+                        (move g (car coords) (cadr coords)))
+                    '((1 1) (3 1) (1 2) (2 2) (3 3) (1 3))
+                    :initial-value (initial-game))))
+        (is (eql 'o-win (game-status game)))))
+
+(test after-nine-moves-without-line-it-is-a-draw
+      (let ((game (reduce 
+                    (lambda (g coords)
+                        (move g (car coords) (cadr coords)))
+                    '((2 1) (1 1) (2 2) (1 2) (3 2) (3 1) (1 3) (2 3) (3 3))
+                    :initial-value (initial-game))))
+        (is (eql 'draw (game-status game)))))
 (run!)
 
